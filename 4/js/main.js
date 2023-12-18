9e7fc753e08c366448b68294221b0914a6fcab2b
Vladislav K <mustlightvk@vk.com>
2023-12-18 19:30:37 +0700

    add func. calculateFinancialValues

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,54 +1,61 @@
-function calculateAvailabeMoney(income, costs){
+function calculateAvailableMoney(income, costs) {
     return income - costs;
 }
-function calculateDailySpend(spend){
-    return(spend/30).toFixed();
+
+function calculateDailySpend(spend) {
+    return (spend / 30).toFixed();
 }
-function calculateAccumulation(totalMonth, totalPercents){
-    return ((totalMonth * totalPercents)/100).toFixed();
+
+function calculateAccumulation(totalMonth, totalPercents) {
+    return ((totalMonth * totalPercents) / 100).toFixed();
 }
-function calculateAnnualSavings(accumulation){
+
+function calculateAnnualSavings(accumulation) {
     return accumulation * 12;
 }
 
-function updateDOMValues(totalMonth, accumulation, totalDay, spend, totalYear){
-    $('#total-month').val(totalMonth);
-    $('#accumulation').val(accumulation);
-    $('#total-day').val(totalDay);
-    $('#spend').val(spend);
-    $('#total-year').val(totalYear);
+function calculateFinancialValues() {
+    const totalPercents = $('#money-box-range').val();
+
+    const income = strToNum($('#income-salary')) + strToNum($('#income-freelance')) + strToNum($('#income-add1')) + strToNum($('#income-add2'));
+    const costs = strToNum($('#cost-flat')) + strToNum($('#cost-house-service')) + strToNum($('#cost-transport')) + strToNum($('#cost-credits'));
+    const totalMonth = calculateAvailableMoney(income, costs);
+    const accumulation = calculateAccumulation(totalMonth, totalPercents);
+    const spend = totalMonth - accumulation;
+    const totalDay = calculateDailySpend(spend);
+    const totalYear = calculateAnnualSavings(accumulation);
+
+    return {
+        totalMonth,
+        accumulation,
+        totalDay,
+        spend,
+        totalYear
+    };
 }
 
-function strToNum(str){
+function updateDOMValues(values) {
+    $('#total-month').val(values.totalMonth);
+    $('#accumulation').val(values.accumulation);
+    $('#total-day').val(values.totalDay);
+    $('#spend').val(values.spend);
+    $('#total-year').val(values.totalYear);
+}
+
+function strToNum(str) {
     return str.val() ? parseInt(str.val()) : 0;
 }
 
-$(document).ready(function(){
-    let totalPercents = 0;
+$(document).ready(function () {
+    $('input').on('input', function () {
+        const financialValues = calculateFinancialValues();
+        updateDOMValues(financialValues);
+    });
 
-    $('input').on('input', function(){
-        const income = strToNum($('#income-salary')) + strToNum($('#income-freelance')) + strToNum($('#income-add1')) + strToNum($('#income-add2'));
-        const costs = strToNum($('#cost-flat')) + strToNum($('#cost-house-service')) + strToNum($('#cost-transport')) + strToNum($('#cost-credits'));
-        const totalMonth = calculateAvailabeMoney(income, costs);
-        const accumulation = calculateAccumulation(totalMonth, totalPercents);
-        const spend = totalMonth - accumulation;
-        const totalDay = calculateDailySpend(spend);
-        const totalYear = calculateAnnualSavings(accumulation)
-        updateDOMValues(totalMonth, accumulation, totalDay, spend, totalYear);
-    })
-
-    $('#money-box-range').on('input', function(){
-        totalPercents = $(this).val();
-        $('#total-percents').html(totalPercents);
-
-        const income = strToNum($('#income-salary')) + strToNum($('#income-freelance')) + strToNum($('#income-add1')) + strToNum($('#income-add2'));
-        const costs = strToNum($('#cost-flat')) + strToNum($('#cost-house-service')) + strToNum($('#cost-transport')) + strToNum($('#cost-credits'));
-        const totalMonth = calculateAvailabeMoney(income, costs);
-        const accumulation = calculateAccumulation(totalMonth, totalPercents);
-        const spend = totalMonth - accumulation;
-        const totalDay = calculateDailySpend(spend);
-        const totalYear = calculateAnnualSavings(accumulation)
-        updateDOMValues(totalMonth, accumulation, totalDay, spend, totalYear);
-    })
-})
+    $('#money-box-range').on('input', function () {
+        const financialValues = calculateFinancialValues();
+        updateDOMValues(financialValues);
+        $('#total-percents').html($('#money-box-range').val());
+    });
+});
 
